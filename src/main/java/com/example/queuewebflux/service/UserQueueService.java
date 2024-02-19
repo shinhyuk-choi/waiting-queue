3c4fe748421f78fc776dfdd4f391b5c2efed21d4
shinhyuk-choi <hyuklassic@gmail.com>
2024-02-19 22:35:53 +0900

    feat: schedule allow user

--- src/main/java/com/example/queuewebflux/service/UserQueueService.java
+++ src/main/java/com/example/queuewebflux/service/UserQueueService.java
@@ -3,10 +3,16 @@ package com.example.queuewebflux.service;
 import com.example.queuewebflux.exception.ErrorCode;
 import java.time.Instant;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueueService {
@@ -14,7 +20,11 @@ public class UserQueueService {
   private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
   private final String USER_QUEUE_WAIT_KEY = "user:queue:%s:wait";
+  private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "user:queue:*:wait";
   private final String USER_QUEUE_PROCEED_KEY = "user:queue:%s:proceed";
+
+  @Value("${scheduler.enabled}")
+  private Boolean scheduling = false;
 
 
   public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
@@ -49,5 +59,25 @@ public class UserQueueService {
         .rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
         .defaultIfEmpty(-1L)
         .map(rank -> rank >= 0 ? rank + 1 : rank);
+  }
+
+  @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+  public void scheduleAllowUser() {
+    if (!scheduling) {
+      return;
+    }
+    var maxAllowUserCount = 3L;
+
+    reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+            .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+            .count(100)
+            .build())
+        .map(key -> key.split(":")[2])
+        .flatMap(
+            queue -> allowUser(queue, maxAllowUserCount).map(allowed -> Tuples.of(queue, allowed)))
+        .doOnNext(tuple -> log.info(
+            "Tried %d and allowed %d members of %s queue".formatted(maxAllowUserCount,
+                tuple.getT2(), tuple.getT1())))
+        .subscribe();
   }
 }
